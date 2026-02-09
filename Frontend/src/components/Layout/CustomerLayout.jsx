627b9a98ff380e29df8e1505b2fcfa95940ba0c1
duyanhad <duyanhvip3k@gmail.com>
2026-02-09 12:42:53 +0700

    chat ui ben khach hang

--- Frontend/src/components/Layout/CustomerLayout.jsx
+++ Frontend/src/components/Layout/CustomerLayout.jsx
@@ -2,13 +2,20 @@ import { Layout } from "antd";
 import AppHeader from "./Header";
 import AppFooter from "./Footer";
 import { Outlet } from "react-router-dom";
+import ChatWidget from "../ChatBox/ChatWidget"; // <-- tạo component này ở src/components/ChatWidget.jsx
 
 const CustomerLayout = () => {
   return (
     <Layout>
       <AppHeader />
+
+      {/* Nội dung trang */}
       <Outlet />
+
       <AppFooter />
+
+      {/* Chat nổi + quick actions */}
+      <ChatWidget />
     </Layout>
   );
 };
